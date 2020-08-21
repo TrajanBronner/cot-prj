18cea4975b71d078ba199558862ab996f65190c6
trajan <trajan.bronner@gmail.com>
2020-08-21 09:47:03 +0200

    feat(routing): add simple routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import PostView from './Views/PostView';
 import UserView from './Views/UserView';
 
-function App() {
+enum allView {
+    Users = 'users',
+    Posts = 'posts',
+}
+
+
+const App = () => {
+
+    const [displayedView, setDisplayedView] = useState(allView.Users);
+
+    const selectView = (view: allView) => {
+        setDisplayedView(view);
+    };
+
     return (
         <div className="App">
             <header className="App-header">
-                <UserView/>
+                <div>
+                    <span className={`m ${displayedView === allView.Posts ? 'bold' : ''} `}
+                          onClick={() => selectView(allView.Posts)}>Posts</span>
+
+                    <span className={`m ${displayedView === allView.Users ? 'bold' : ''} `}
+                          onClick={() => selectView(allView.Users)}>Users</span>
+                </div>
             </header>
+
+            {displayedView === allView.Posts && <PostView/>}
+
+            {displayedView === allView.Users && <UserView/>}
+
         </div>
     );
-}
+};
 
 export default App;
---
+++ src/Views/PostView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PostView: React.FunctionComponent<any> = (props) => {
+
+    return (
+        <React.Fragment>
+            WIP
+        </React.Fragment>
+    );
+};
+
+export default PostView;
